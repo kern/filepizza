1e297c33c694bf7ef5700f8276da368c002c4056
Neeraj Baid <wdaareg@gmail.com>
2015-02-25 17:55:33 -0800

    Remove a comment. Add bundle.js.

--- client/bundle.js
+++ client/bundle.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var files = []
 
 function handleFileSelect(evt) {
@@ -5,6 +6,8 @@ function handleFileSelect(evt) {
   evt.preventDefault();
 
   files = evt.dataTransfer.files; // FileList object.
+
+  // files is a FileList of File objects. List some properties.
   var output = [];
       metadata = [];
   for (var i = 0, file; file = files[i]; i++) {
@@ -20,13 +23,11 @@ function handleFileSelect(evt) {
   }
   document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
   var firebase = new Firebase("https://webdrop.firebaseio.com/");
-  var id = createID()
-  firebase.set({
-    id : {
-      "id" : id,
-      "file_metadata" : metadata
-    }
-  })
+  // firebase.set({
+  //   "id" : createID(),
+  //   "file_metadata" : metadata
+  // })
+  // post metadata to server
 }
 
 function send() { // perhaps need params for peer id or something
@@ -72,3 +73,5 @@ function createID() {
 var dropZone = document.getElementById('drop_zone');
 dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', handleFileSelect, false);
+
+},{}]},{},[1]);
