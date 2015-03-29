cde94167d2bb18a2873a943c7df25dd8258e117f
Neeraj Baid <wdaareg@gmail.com>
2015-03-29 12:04:17 -0700

    Remove file size and type descriptors.

--- client/components/FileDescription.js
+++ client/components/FileDescription.js
@@ -5,8 +5,6 @@ export default class FileDescription extends React.Component {
   render() {
     return <div className="file-description">
       <span className="file-name">{this.props.file.name}</span>
-      <span className="file-size">{this.props.file.size}</span>
-      <span className="file-type">{this.props.file.type}</span>
     </div>;
   }
 
