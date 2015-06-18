db4a9bf278d9d09f292ff87a849599899d75d270
Alex Kern <alex@kern.io>
2015-06-17 23:59:26 -0700

    Update to babel 5.x.x.
    Fixes #13.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
-require('babel/register')
+
+require('babel/register')({
+  only: new RegExp(__dirname + '/lib' + '|' +
+                   __dirname + '/node_modules/filepizza')
+})
+
 module.exports = require('./lib/server')
