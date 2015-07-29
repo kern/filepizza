b4cec71c03f6438c6f1035fbe09a8ccaafd5bc03
Alex Kern <alex@kern.io>
2015-07-28 22:53:20 -0700

    Add New Relic.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+try {
+  require('./newrelic')
+  require('newrelic')
+} catch (ex) {
+  // Don't load New Relic if the configuration file doesn't exist.
+}
+
 require('babel/register')({
   only: new RegExp(__dirname + '/lib' + '|' +
                    __dirname + '/node_modules/filepizza')
