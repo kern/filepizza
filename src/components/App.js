332afc7d852f6c3eeb15f9bc81ac3365228f94de
Alex Kern <alex@kern.io>
2016-10-17 16:13:00 -0400

    Use official WebTorrent CDN

--- src/components/App.js
+++ src/components/App.js
@@ -44,7 +44,7 @@ export default class App extends React.Component {
         <link rel="stylesheet" href="/app.css" />
 
         <Bootstrap data={this.props.data} />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/webtorrent/0.97.2/webtorrent.min.js" />
+        <script src="https://cdn.jsdelivr.net/webtorrent/latest/webtorrent.min.js" />
         <script src="/app.js" />
 
       </FrozenHead>
