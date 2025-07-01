ee6acbbfb25a5c5ac11309c42967d789dc5392b3
Alex Kern <alex@kern.io>
2025-06-30 21:38:42 -0700

    Configure playwright web server for container

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,13 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './tests/e2e',
   webServer: {
-    command: 'PORT=4100 node .next/standalone/server.js',
+    command: 'HOSTNAME=127.0.0.1 PORT=4100 node .next/standalone/server.js',
     port: 4100,
     timeout: 120 * 1000,
     reuseExistingServer: true,
+  },
+  use: {
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
   },
 })
